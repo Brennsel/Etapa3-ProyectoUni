28dc25efa211f1ae4afba867ecad441f0fa61bec
Brennsel <brennsell@gmail.com>
2023-11-10 23:06:48 -0300

    Se creo forms.py

---
+++ proyectouni/appuni/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Alumno, MATERIAS
+
+
+class FormularioAlumno(forms.ModelForm):
+    class Meta:
+        model = Alumno
+        fields = '__all__'
+        labels = {
+            'id': 'ID',
+            'nombre': 'Nombre',
+            'edad': 'Edad',
+            'materia': 'Materia',
+        }
+
+
+    materia = forms.ChoiceField(
+        choices=MATERIAS, 
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+    )
